affe6b77bcdab732b5eae4907424aa4cacbb32ca
Raihan Firmansyah <raihan.firm@gmail.com>
2025-05-11 09:48:23 +0700

    fix: mig version

---
+++ back-end/migrations/000002_init_tables.up.sql
@@ -0,0 +1,110 @@
+
+CREATE TABLE IF NOT EXISTS product_stock_detail (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    product_id UUID NOT NULL,
+    size VARCHAR(10) NOT NULL,
+    stock DECIMAL NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_product
+        FOREIGN KEY (product_id)
+        REFERENCES product (id)
+        ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS carts (
+  id UUID PRIMARY KEY,
+  user_id UUID NOT NULL,
+  total_price DECIMAL(10, 2) DEFAULT 0,
+  total_quantity INT,
+  total_weight FLOAT,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS cart_items (
+  id UUID PRIMARY KEY,
+  cart_id UUID REFERENCES carts(id),
+  product_id UUID NOT NULL,
+  size VARCHAR NOT NULL,
+  quantity INT NOT NULL,
+  weight FLOAT NOT NULL,
+  price DECIMAL(10, 2) NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE cusProduct (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id UUID NOT NULL,
+    name VARCHAR(255),
+    description TEXT,
+    price DECIMAL NOT NULL,
+    status VARCHAR(255) DEFAULT 'pending',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      FOREIGN KEY(user_id)
+      REFERENCES customer(id)
+      ON DELETE CASCADE
+);
+
+CREATE TABLE customer_product_files (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    product_id UUID NOT NULL,
+    url VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      FOREIGN KEY(product_id) 
+      REFERENCES cusProduct(id)
+      ON DELETE CASCADE
+);
+
+CREATE TABLE message (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    product_id UUID NOT NULL,
+    user_id UUID NOT NULL,
+    message text,
+    role VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      FOREIGN KEY(product_id) 
+      REFERENCES cusProduct(id)
+      ON DELETE CASCADE,
+);
+
+CREATE TABLE transactions (
+    id UUID PRIMARY KEY,
+    user_id UUID NOT NULL,
+    total_amount DECIMAL(12,2) NOT NULL,
+    payment_method VARCHAR(50) NOT NULL DEFAULT 'Bank Transfer',
+    payment_proof VARCHAR(255) NOT NULL,
+    status VARCHAR(20) DEFAULT 'pending' CHECK (status IN ('pending', 'proccess', 'canceled', 'success')),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES customer(id)
+);
+
+CREATE TABLE shipping_address (
+    id UUID PRIMARY KEY,
+    transaction_id UUID NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    phone_number VARCHAR NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    zip_code VARCHAR(255) NOT NULL,
+    destination_label VARCHAR(255) not null,
+    courir VARCHAR(255) not null,
+    shpiing_cost DECIMAL(12,2) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (transaction_id) REFERENCES transactions(id)
+);
+
+CREATE TABLE transaction_details (
+    id UUID PRIMARY KEY,
+    transaction_id UUID NOT NULL,
+    product_id VARCHAR(255) NOT NULL,
+  	size VARCHAR(255) NOT NULL,
+  	quantity INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (transaction_id) REFERENCES transactions(id)
+);
