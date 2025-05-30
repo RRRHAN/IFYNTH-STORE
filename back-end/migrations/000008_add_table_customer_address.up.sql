6eb236b90a28705dcf0f194ea61eac05496c13d0
Raihan Firmansyah <raihan.firm@gmail.com>
2025-05-30 21:27:42 +0700

    feat(mig): add table and column

---
+++ back-end/migrations/000008_add_table_customer_address.up.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS customer_address (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    customer_id UUID NOT NULL,
+    destination_id VARCHAR(255),
+    address VARCHAR(255),
+    zip_code VARCHAR(255),
+    destination_label VARCHAR(255),
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    FOREIGN KEY (customer_id) REFERENCES customer(id)
+);
