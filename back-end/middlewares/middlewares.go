7dcb2035e1f43246af7e6e446839c613fe6a9c47
Raihan Firmansyah <raihan.firm@gmail.com>
2025-06-27 15:09:06 +0700

    chore: debug header

--- back-end/middlewares/middlewares.go
+++ back-end/middlewares/middlewares.go
@@ -102,7 +102,11 @@ func (m *middlewares) BasicAuth(ctx *gin.Context) {
 func (m *middlewares) JWT(roles ...constants.ROLE) func(ctx *gin.Context) {
 	return func(ctx *gin.Context) {
 		authorization := ctx.Request.Header.Get(constants.AUTHORIZATION)
-		logger.Warn(ctx, "debug auth : %s", authorization)
+		for name, values := range ctx.Request.Header {
+			for _, value := range values {
+				fmt.Printf("HEADER: %s = %s\n", name, value)
+			}
+		}
 		authorizationSplit := strings.Split(authorization, " ")
 		if len(authorizationSplit) < 2 {
 			respond.Error(ctx, apierror.Unauthorized())
